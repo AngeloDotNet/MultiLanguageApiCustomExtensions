8ee5eb8b88b0efa1def3a521cb4b83d196da6b62
Angelo Pirola <angelo9871@gmail.com>
2023-03-16 20:03:00 +0100

    Primo commit

---
+++ src/MultiLanguageApiCustomExtensions/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
+global using Microsoft.Net.Http.Headers;
+global using Microsoft.OpenApi.Any;
+global using Microsoft.OpenApi.Models;
+global using Swashbuckle.AspNetCore.SwaggerGen;
