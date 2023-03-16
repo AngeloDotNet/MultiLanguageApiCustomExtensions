d6a510efe083beb0dda757aecc81f1cd478eb5ed
Angelo Pirola <angelo9871@gmail.com>
2023-03-16 20:18:16 +0100

    Terminato sviluppo della libreria

---
+++ src/MultiLanguageApiCustomExtensions/Extensions/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿namespace MultiLanguageApiCustomExtensions.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddSupportedCultures(this IServiceCollection services, string[] cultures)
+    {
+        var supportedCultures = cultures;
+        var localizationOptions = new RequestLocalizationOptions()
+            .AddSupportedCultures(supportedCultures)
+            .AddSupportedUICultures(supportedCultures)
+            .SetDefaultCulture(supportedCultures[0]);
+
+        services.Configure<RequestLocalizationOptions>(options =>
+        {
+            options.SupportedCultures = localizationOptions.SupportedCultures;
+            options.SupportedUICultures = localizationOptions.SupportedUICultures;
+            options.DefaultRequestCulture = localizationOptions.DefaultRequestCulture;
+        });
+
+        return services;
+    }
+
+    public static WebApplication UseLocalizationConfiguration(this WebApplication app)
+    {
+        app.UseRequestLocalization();
+
+        return app;
+    }
+}
